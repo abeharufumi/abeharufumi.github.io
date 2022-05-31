495f5d0308e8ccaf0f89ab8c74ea61a660920e76
abeharufumi <98261876+abeharufumi@users.noreply.github.com>
2022-05-31 23:35:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
   const overlay = document.querySelector('.overlay');
   const closeIcon = document.getElementById('closeIcon');
   
+  
   MenuIcon.addEventListener('click', () => {
     overlay.classList.add('show');
     MenuIcon.classList.add('hide');
@@ -20,11 +21,15 @@
   // IntersectionObserver API
   const scrollObserver = new IntersectionObserver(scrollCallback);
   const apperObserver = new IntersectionObserver(apperCallback);
+
+  
   scrollObserver.observe(document.getElementById('target'));
   apperObserver.observe(document.getElementById('target'));
+  
   const header = document.querySelector('header');
   const up = document.getElementById('up')
-
+  
+  
   function scrollCallback(empties) {
     empties.forEach(empty => {
       if (!empty.isIntersecting) {
@@ -52,5 +57,29 @@
       behavior: "smooth",
     });
   });
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+  
 
 }
